d51c1913f51ebfc780e16acd7a2310a8bb5f3121
liudeyao <766592805@qq.com>
2026-09-13 15:53:27 +0800

    chore: untrack and remove build backup artifacts

--- dist_bak/.prerender/chunks/compiler_CLriVEbg.mjs
+++
@@ -1,2 +0,0 @@
-import "./jsx-runtime_BecIScU7.mjs";
-export {};
--- dist_bak/.prerender/chunks/content-modules_CoOPiwqC.mjs
+++
@@ -1,9 +0,0 @@
-//#region .astro/content-modules.mjs
-var content_modules_default = /* @__PURE__ */ new Map([
-	["src/content/posts/_color-schemes/predefined-color-schemes.mdx", () => import("./predefined-color-schemes_0O-LBLh6.mjs")],
-	["src/content/posts/adding-new-post.mdx", () => import("./adding-new-post_YXcBrFU0.mjs")],
-	["src/content/posts/customizing-astropaper-theme-color-schemes.mdx", () => import("./customizing-astropaper-theme-color-schemes_CZItXcej.mjs")],
-	["src/content/posts/how-to-configure-astropaper-theme.mdx", () => import("./how-to-configure-astropaper-theme_DTXuGHaM.mjs")]
-]);
-//#endregion
-export { content_modules_default as default };
